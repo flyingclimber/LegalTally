801f4bbbbda5f932be8d42363f81d70c9f92e7e6
Tomasz Finc <tomasz@gmail.com>
2012-04-16 06:21:36 +0000

    Pulling values from DB for message update. Moving message generation to plus_one(). First fully working version!

--- legaltally.py
+++ legaltally.py
@@ -43,7 +43,7 @@ def teardown_request(exception):
 @app.route('/')
 def show_tally():
     cur = g.db.execute('select approved, denied from tally')
-    entries = [dict(approved=row[0], denied=[row[1]]) for row in cur.fetchall()]
+    entries = [dict(approved=row[0], denied=row[1]) for row in cur.fetchall()]
     return render_template('show_tally.html', entries=entries)
 
 @app.route('/plus_one/<increment>', methods=['GET'])
@@ -53,18 +53,21 @@ def plus_one(increment):
     if increment == 'denied':
         g.db.execute('update tally set denied = denied + 1 where id = 1') 
     g.db.commit()
+
     flash('Tally Updated')
-    update_sign();
+    cur = g.db.execute('select approved, denied from tally')
+    entries = [dict(approved=row[0], denied=row[1]) for row in cur.fetchall()]
+    update_sign("Approved: %s Denied: %s" % ( entries[0]['approved'], entries[0]['denied']) );
     return redirect(url_for('show_tally'))
 
 ### End Web Code ###
 
 ### Serial Code ###
-def update_sign():
+def update_sign(message):
     ser = serial.Serial('/dev/ttyUSB0', 9600)
     ser.write('<ID01>\r\n')
     time.sleep(1)
-    ser.write('<ID01><PA><FQ><CC>Approved: X Denied: X\r\n')
+    ser.write('<ID01><PA><FQ><CC> %s \r\n' % message)
     ser.close()
 
 #### End Serial Code ###
