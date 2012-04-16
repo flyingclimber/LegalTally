fd7b00dd9ca36314bec135a98564e8382e6535d9
Tomasz Finc <tomasz@gmail.com>
2012-04-16 00:48:39 -0700

    Passing through pylint

--- legaltally.py
+++ legaltally.py
@@ -1,7 +1,7 @@
-# all the imports
+'''Legal Tally -  A simple controller for counting take down requests and
+    displaying them on an LED SIGN'''
 import sqlite3, serial, time
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+from flask import Flask, g, redirect, url_for, render_template, flash
 from contextlib import closing
 
 # configuration
@@ -17,38 +17,48 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 ### DB section ###
+def init_db():
+    '''Create the initial db'''
+    with app.open_resource('schema.sql') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
 def connect_db():
+    '''Open a db connection'''
     return sqlite3.connect(app.config['DATABASE'])
 
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+### end db section ###
 
-### End DB Section ###
-
-### Web Code ###
+### web code ###
 @app.before_request
 def before_request():
+    '''open a db connection before the web view is served'''
     g.db = connect_db()
 
 @app.teardown_request
 def teardown_request(exception):
+    '''close the db handle after the web request is over'''
+    if exception:
+        print exception
     g.db.close()
 
 @app.errorhandler(404)
 def not_found(error):
+    '''404 handler'''
+    if error:
+        print error
     return render_template('error.html'), 404
 
 @app.route('/')
 def show_tally():
+    '''Show the default tally web view'''
     cur = g.db.execute('select approved, denied from tally')
     entries = [dict(approved=row[0], denied=row[1]) for row in cur.fetchall()]
     return render_template('show_tally.html', entries=entries)
 
 @app.route('/plus_one/<increment>', methods=['GET'])
 def plus_one(increment):
+    '''Increment either the approved or denied counter and update the sign'''
     if increment == 'approved':
         g.db.execute('update tally set approved = approved + 1 where id = 1')
     if increment == 'denied':
@@ -58,11 +68,13 @@ def plus_one(increment):
     flash('Tally Updated')
     cur = g.db.execute('select approved, denied from tally')
     entries = [dict(approved=row[0], denied=row[1]) for row in cur.fetchall()]
-    update_sign("Approved: %s Denied: %s" % (entries[0]['approved'], entries[0]['denied']));
+    update_sign("Approved: %s Denied: %s" % 
+            (entries[0]['approved'], entries[0]['denied']))
     return redirect(url_for('show_tally'))
 
 @app.route('/reset')
 def reset():
+    '''Reset the counter and update the sign'''
     g.db.execute('update tally set approved = 0, denied = 0 where id = 1')
     g.db.commit()
     flash('Tally Reset')
@@ -73,6 +85,7 @@ def reset():
 
 ### Serial Code ###
 def update_sign(message):
+    '''Take the incoming message and send it to the sign'''
     ser = serial.Serial(DEVICE, BAUD_RATE)
     ser.write('<ID01>\r\n')
     time.sleep(1)
