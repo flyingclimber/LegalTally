ca3c49a62a92558f104b6318a3141d9eee980e3f
Tomasz Finc <tomasz@gmail.com>
2012-04-29 21:37:57 -0700

    Adding license info

--- legaltally.py
+++ legaltally.py
@@ -1,5 +1,24 @@
-'''Legal Tally -  A simple controller for counting take down requests and
-    displaying them on an LED SIGN'''
+'''  
+  LegalTally -- LegalTally is python flask based application that takes 
+  user input and sends it to a ProLite Led sign over serial
+ 
+  Copyright (C) 2012, Tomasz Finc <tomasz@gmail.com>
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'''
+
 import sqlite3, serial, time
 from flask import Flask, g, redirect, url_for, render_template, flash
 from contextlib import closing
