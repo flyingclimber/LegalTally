05d43b12530da73bf427e5c31f95a505c476f886
Tomasz Finc <tomasz@gmail.com>
2012-04-29 18:58:11 -0700

    Removing unusded Controller.py

--- Controller.py
+++
@@ -1,7 +0,0 @@
-# As a user I want to be able to update the amount of content takedown requests and takedown refused on an led sign through a web browser
-#
-# 1) Display Web Page with + 1 Takedown Request + 1 Takedown Refused 
-# 2) Web call adds a +1 to internal db of both
-# 3) Web calls +1 to be sent to the sign
-# 4) Sign recieves + 1 and notes what message style its currently using
-# 5) Updated count is sent over serial
